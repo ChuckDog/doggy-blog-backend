d8f95a7ad147b09cbad22db6a804000134cfb9bc
chuckdoggy <xuzhong2012@my.fit.edu>
2026-01-30 17:58:21 +0800

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,47 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import cookieParser from 'cookie-parser';
+import {urlencoded, json} from 'express';
+import helmet from 'helmet';
+import {DocumentBuilder, SwaggerModule, SwaggerCustomOptions} from '@nestjs/swagger';
+
+const enum Environment {
+  Development = 'development',
+  Production = 'production',
+}
+const environment = process.env.ENVIRONMENT ?? Environment.Development;
+const port = parseInt(process.env.PORT ?? '') || 3000;
+const allowedOrigins = (process.env.ALLOWED_ORIGINS ?? '').split(',');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(cookieParser());
+  app.use(json({limit: '10mb'}));
+  app.use(urlencoded({limit: '10mb', extended: true}));
+  app.enableCors({credentials: true, origin: allowedOrigins});
+  if (environment === Environment.Production) {
+    app.use(helmet());
+  } else if (environment === Environment.Development) {
+    const config = new DocumentBuilder()
+      .setTitle('API Document')
+      .setDescription("It's good to see you guys 🥤")
+      .setVersion('1.0')
+      .addCookieAuth('refreshToken')
+      .addBearerAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    const customOptions: SwaggerCustomOptions = {
+      explorer: true,
+      swaggerOptions: {
+        persistAuthorization: true,
+        tagsSorter: 'alpha',
+      },
+      customSiteTitle: 'API Document',
+    };
+    SwaggerModule.setup('api', app, document, customOptions);
+  }
+
+  await app.listen(port ?? 3000);
 }
 bootstrap();
